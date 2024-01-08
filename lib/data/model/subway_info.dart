6087414d541b2e3269c3a85461a5f264b0314a10
offyushin <offyushin@naver.com>
2024-01-08 23:30:05 +0900

    feat: ui 수정 ing

--- lib/data/model/subway_info.dart
+++ lib/data/model/subway_info.dart
@@ -8,13 +8,13 @@ part 'subway_info.g.dart';
 @freezed
 class SubwayInfo with _$SubwayInfo {
   const factory SubwayInfo({
-    required String? updnLine,
-    required String? statnNm,
-    required String? trainLineNm,
-    required String? arvlMsg2,
-    required String? arvlMsg3,
-    required String? trnsitCo,
-    required String? barvlDt,
+    required String? updnLine, // 상하행 구분
+    required String? statnNm, // 지하철역명
+    required String? trainLineNm, // 도착지 방면
+    required String? arvlMsg2, // 도착,출발 진입 등
+    required String? arvlMsg3, // oo도착, 12분후 oo
+    required String? trnsitCo, // 환승 노선
+    required String? barvlDt, // 도착 예정 시간
   }) = _SubwayInfo;
 
   factory SubwayInfo.fromJson(Map<String, Object?> json) =>
--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -22,7 +22,9 @@ class _MainScreenState extends State<MainScreen> {
     final viewModel = context.watch<MainViewModel>();
     final state = viewModel.state;
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.grey[500],
         title: const Text('서울지하철 정보 조회'),
       ),
       body: Column(
@@ -42,7 +44,26 @@ class _MainScreenState extends State<MainScreen> {
             child: ListView(
               children: state.subwayInfoList
                   .map(
-                    (e) => Text(e.toString()),
+                    (e) => Column(
+                      children: [
+                        ListTile(
+                          leading: Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[600],
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Center(child: Text(e.statnNm!)),
+                          ),
+                          title: Text((e.statnNm!)),
+                          subtitle: Text(e.trainLineNm!),
+                          trailing: Container(
+                            child: Text(e.barvlDt!),
+                          ),
+                        ),
+                      ],
+                    ),
                   )
                   .toList(),
             ),
