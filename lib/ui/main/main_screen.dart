44cfe8be1975ea1e90d0aa231d25b081946001d6
jeong won <jw221356@naver.com>
2024-01-08 19:05:53 +0900

    feat: 뼈대작성

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
