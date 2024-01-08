199227f2e12ecbd256409a23e2694fd33afad91d
jeong won <jw221356@naver.com>
2024-01-08 19:24:13 +0900

    feat: UI 생서엉~

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -5,6 +5,30 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('서울지하철 정보 조회'),
+      ),
+      body: Column(
+        children: [
+          Text('지하철 역 이름'),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: '지하철 역 이름',
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Text('111'),
+                Text('111'),
+                Text('111'),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
